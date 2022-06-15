afb5f8d0b46ceddfd9a0c4c0b7845a947336a886
Lucas Coronel <lccoronel7@icloud.com>
2022-06-15 18:00:50 -0300

    ✨ authenticate client use case

---
+++ src/modules/account/useCases/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,28 @@
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+import { prisma } from "@database/prismaClient"
+
+interface IAuthenticateClient {
+  username: string;
+  password: string;
+}
+
+export class AuthenticateClientUseCase {
+  async execute({ username, password }: IAuthenticateClient) {
+    const client = await prisma.clients.findFirst({ where: { username } })
+
+    if (!client) throw new Error("Username or password is invalid")
+
+    const passwordMatch = await compare(password, client.password)
+
+    if (!passwordMatch) throw new Error("Username or password is invalid")
+
+    const token = sign({ username }, "7108537956afa2a526f96cc9da7b0c36", {
+      subject: client.id,
+      expiresIn: "1d"
+    })
+
+    return { token }
+  }
+}
