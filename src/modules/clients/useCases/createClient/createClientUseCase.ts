02d110b806292fc57d5b3aa9a32b2fee1a33aa3b
Lucas Coronel <lccoronel7@icloud.com>
2022-06-15 18:00:15 -0300

    ✨ create client use case

---
+++ src/modules/clients/useCases/createClient/createClientUseCase.ts
@@ -0,0 +1,27 @@
+import { Clients } from '@prisma/client'
+import { hash } from 'bcrypt'
+
+import { prisma } from "@database/prismaClient"
+
+interface ICreteClient {
+  username: string
+  password: string
+}
+
+export class CreateClientUseCase {
+  async execute({ username, password }: ICreteClient): Promise<Clients> {
+    const clientExist = await prisma.clients.findFirst({ 
+      where: { username: { mode: 'insensitive' } } 
+    })
+
+    if (clientExist) throw new Error("Client already exists");
+
+    const hashPassword = await hash(password, 10)
+
+    const client = await prisma.clients.create({
+      data: { username, password: hashPassword }
+    })
+
+    return client
+  }
+}
