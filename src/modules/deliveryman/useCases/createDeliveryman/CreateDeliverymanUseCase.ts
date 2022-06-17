4856b42326c2346a032ec221024eea308a2e0031
Lucas Coronel <lccoronel7@icloud.com>
2022-06-17 08:41:29 -0300

    ✨ deliveryman usecase

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,27 @@
+import { hash } from 'bcrypt'
+import { Deliveryman } from '@prisma/client'
+
+import { prisma } from "@database/prismaClient"
+
+interface ICreteDeliveryman {
+  username: string
+  password: string
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreteDeliveryman): Promise<Deliveryman> {
+    const deliverymanExist = await prisma.deliveryman.findFirst({ 
+      where: { username: { mode: 'insensitive' } } 
+    })
+
+    if (deliverymanExist) throw new Error("Deliveryman already exists");
+
+    const hashPassword = await hash(password, 10)
+
+    const deliveryman = await prisma.deliveryman.create({
+      data: { username, password: hashPassword }
+    })
+
+    return deliveryman
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 
 import { CreateClientController } from "@modules/clients/useCases/createClient/createClientController";
 import { AuthenticateClientController } from "@modules/account/useCases/authenticateClient/AuthenticateClientController";
+import { CreateDeliverymanController } from "@modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "@modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
+const createDeliverymanController = new CreateDeliverymanController()
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 
 routes.post('/clients', createClientController.handle)
-routes.post('/authenticate', authenticateClientController.handle)
+routes.post('/clients/authenticate', authenticateClientController.handle)
+routes.post('/deliveryman', createDeliverymanController.handle)
+routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
 
 export { routes }
