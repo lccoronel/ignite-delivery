40f77a455338d19b1ac0cbca8ec3a9003adfc7e9
Lucas Coronel <lccoronel7@icloud.com>
2022-06-19 11:15:39 -0300

    ✨ create delivery use case

---
+++ src/modules/delivery/useCases/createDelivery/CreateDeliveryUseCase.ts
@@ -0,0 +1,15 @@
+import { Delivery } from "@prisma/client"
+import { prisma } from "@database/prismaClient"
+
+interface ICreateDelivery {
+  item_name: string
+  id_client: string
+}
+
+export class CreateDeliveryUseCase {
+  async execute({ item_name, id_client }: ICreateDelivery): Promise<Delivery> {
+    const delivery = await prisma.delivery.create({ data: { item_name, id_client } })
+
+    return delivery
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateClientController } from "@modules/clients/useCases/createClient/c
 import { AuthenticateClientController } from "@modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { CreateDeliverymanController } from "@modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 import { AuthenticateDeliverymanController } from "@modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { CreateDeliveryController } from "@modules/delivery/useCases/createDelivery/CreateDeliveryController";
 
 const routes = Router()
 
@@ -11,10 +12,12 @@ const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const createDeliverymanController = new CreateDeliverymanController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const createDeliveryController = new CreateDeliveryController()
 
 routes.post('/clients', createClientController.handle)
 routes.post('/clients/authenticate', authenticateClientController.handle)
 routes.post('/deliveryman', createDeliverymanController.handle)
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle)
+routes.post('/delivery', createDeliveryController.handle)
 
 export { routes }
