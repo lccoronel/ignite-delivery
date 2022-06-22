2b12e25246430a397c4a2a417c298a3c14520c9d
Lucas Coronel <lccoronel7@icloud.com>
2022-06-22 11:14:37 -0300

    ♻️ apply select on find deliveries

--- src/modules/clients/useCases/findDeliveriesByClient/FindDeliveriesByClientUseCase.ts
+++ src/modules/clients/useCases/findDeliveriesByClient/FindDeliveriesByClientUseCase.ts
@@ -4,7 +4,7 @@ export class FindDeliveriesByClientUseCase {
   async execute(id_client: string) {
     const deliveries = await prisma.clients.findMany({
       where: { id: id_client },
-      include: { delivery: true }
+      select: { delivery: true, username: true, id: true }
     })
 
     return deliveries
